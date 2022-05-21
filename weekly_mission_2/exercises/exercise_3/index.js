dd8b7040d9a826407ea3e1482768901d548e0a9e
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-05-21 12:37:06 -0500

    Ejercicio Completado

---
+++ weekly_mission_2/exercises/exercise_3/index.js
@@ -0,0 +1,7 @@
+import * as Twitter from './Twitter/index.js'
+import * as Facebook from './Facebook/index.js';
+import * as Uber from './Uber/index.js';
+
+console.log(Twitter, Facebook, Uber);
+
+export { Twitter, Facebook, Uber };
