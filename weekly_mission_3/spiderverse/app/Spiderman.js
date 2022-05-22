4f27b12f3ad48314d2478c671a4ede1da2dbde5a
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-05-22 14:05:29 -0500

    Spiderman class added. Test case for Spiderman class created

---
+++ weekly_mission_3/spiderverse/app/Spiderman.js
@@ -0,0 +1,28 @@
+class Spiderman{
+    #name;
+    #age;
+    #actor;
+    #movies;
+    #studio;
+
+    constructor(name, age, actor, movies, studio){
+        this.#name = name;
+        this.#age = age;
+        this.#actor = actor;
+        this.#movies = movies;
+        this.#studio = studio;
+    }
+
+    getName(){ return this.#name; }
+    setName(name){ this.#name = name; }
+    getAge(){ return this.#age; }
+    setAge(age){ this.#age = age; }
+    getActor(){ return this.#actor; }
+    setActor(actor){ this.#actor = actor; }
+    getMovies(){ return this.#movies; }
+    setMovies(movies){ this.#movies = movies; }
+    getStudio(){ return this.#studio; }
+    setStudio(studio){ this.#studio = studio; }
+}
+
+module.exports = Spiderman;
--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,12 @@
+const Spiderman = require("../app/Spiderman");
+
 describe('First suit case for spiderman test', () => {
-    test('Case 1. Dummy', () => {
-        const resultOfSomething = 1 + 2;
-        expect(resultOfSomething).toBe(1);
+    test('Case 1. Creating a new instance of Spiderman class', () => {
+        const inst = new Spiderman("Peter Parker", 16, "Tom Holland", 4, "Marvel Studios");
+        expect(inst.getName()).toBe(1);
+        expect(inst.getAge()).toBe(1);
+        expect(inst.getActor()).toBe(1);
+        expect(inst.getMovies()).toBe(1);
+        expect(inst.getStudio()).toBe(1);
     });
 });
