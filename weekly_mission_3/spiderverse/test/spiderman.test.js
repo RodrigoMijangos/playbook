7717cf1550856a15df29d9a2014026fbcd23d47e
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-05-21 21:20:22 -0500

    Added test case failed for practice porpuses

---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,6 @@
+describe('First suit case for spiderman test', () => {
+    test('Case 1. Dummy', () => {
+        const resultOfSomething = 1 + 2;
+        expect(resultOfSomething).toBe(1);
+    });
+});
