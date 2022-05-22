8afb2d7e5436486f63c2ca0a97f9cf0c022c97ec
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-05-22 15:27:45 -0500

    Test suit passed for spiderman class

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -3,10 +3,10 @@ const Spiderman = require("../app/Spiderman");
 describe('First suit case for spiderman test', () => {
     test('Case 1. Creating a new instance of Spiderman class', () => {
         const inst = new Spiderman("Peter Parker", 16, "Tom Holland", 4, "Marvel Studios");
-        expect(inst.getName()).toBe(1);
-        expect(inst.getAge()).toBe(1);
-        expect(inst.getActor()).toBe(1);
-        expect(inst.getMovies()).toBe(1);
-        expect(inst.getStudio()).toBe(1);
+        expect(inst.getName()).toBe("Peter Parker");
+        expect(inst.getAge()).toBe(16);
+        expect(inst.getActor()).toBe("Tom Holland");
+        expect(inst.getMovies()).toBe(4);
+        expect(inst.getStudio()).toBe("Marvel Studios");
     });
 });
