68e784504f795a826e373a5204bdb9e079654585
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-06-07 20:12:53 -0500

    Twitter Module: Model user is ready (07/06/2022)

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -10,15 +10,17 @@ describe('Suit test for User model: All parameters are strings.', () => {
         expect(usr.getName()).toBe("name");
         expect(usr.getUsername()).toBe("username");
         expect(usr.getBio()).toBe("bio");
+        //Testing if dates are created.
         expect(usr.getDateCreated()).not.toBeUndefined();
         expect(usr.getLastUpdated()).not.toBeUndefined();
     });
     test('Case 2: Parameters are set with "real" parameters', () => {
-        const usr = new User("1", "Rodrigo Mijangos", "rodrigoMijangos", "Software developer in last grades of formation");
+        const usr = new User("1", "Rodrigo Mijangos", "rodrigoMijangos", "Software developer in last grades of formation", "may 24th, 2022", "may 24th, 2022");
 
         expect(usr.getId()).toBe("1");
         expect(usr.getName()).toBe("Rodrigo Mijangos");
-        expect(usr.getUsername()).toBe("rodrigoMijangos"); //Testing if Jest is uppercase sensible
+        expect(usr.getUsername()).toBe("rodrigoMijangos"); //Testing if Jest is uppercase sensible;
+        //expect(usr.getUsername()).toBe("rodrigomijangos"); It was
         expect(usr.getBio()).toBe("Software developer in last grades of formation");
         expect(usr.getDateCreated()).not.toBe("may 24th, 2022");
         expect(usr.getLastUpdated()).not.toBe("may 24th, 2022");
@@ -26,6 +28,15 @@ describe('Suit test for User model: All parameters are strings.', () => {
 });
 describe('Suit test for User model: Created and lastUpdated are Dates.', () => {
     test('Case 1: ', () => {
-        const usr = new User();
+        const usr = new User("1", "Rodrigo Mijangos", "rodrigoMijangos", "Software developer in last grades of formation");
+
+        expect(usr.getId()).toBe("1");
+        expect(usr.getName()).toBe("Rodrigo Mijangos");
+        expect(usr.getUsername()).toBe("rodrigoMijangos");
+        expect(usr.getBio()).toBe("Software developer in last grades of formation");
+        expect(usr.getDateCreated()).not.toBeUndefined();
+        expect(usr.getLastUpdated()).not.toBeUndefined();
+        console.log(usr.getDateCreated())
+        console.log(usr.getLastUpdated())
     });
 });
