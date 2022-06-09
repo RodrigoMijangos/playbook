f63268de45b73a91fd6ea5ab2e68b3e58c7ba9e7
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2022-06-08 22:51:46 -0500

    Services Module: User Service update username and get usernames from list of users

--- weekly_mission_3/UserServiceProject/app/UserService.js
+++ weekly_mission_3/UserServiceProject/app/UserService.js
@@ -8,6 +8,15 @@ class UserService{
     static getInfo(user){
         return [user.getId(), user.getName(), user.getUsername(), user.getBio(), user.getDateCreated(), user.getLastUpdated()];
     }
+
+    static updateUsername(username, user){
+        user.setUsername(username);
+        return user;
+    }
+
+    static getAllUsernames(users){
+        return users.map((user) => user.getUsername());
+    }
 }
 
 module.exports = UserService;
--- weekly_mission_3/UserServiceProject/test/UserService.test.js
+++ weekly_mission_3/UserServiceProject/test/UserService.test.js
@@ -20,4 +20,31 @@ describe('Test case for User Service', () => {
         expect(userInfo[4]).not.toBeUndefined();
         expect(userInfo[5]).not.toBeUndefined();
     });
+    test('3. Updating a username from Service.', () => {
+        const user = _userService.create(1, "Rodrigo Mijangos", "RodrigoMijangos");
+
+        expect(user.getUsername()).toBe("RodrigoMijangos");
+
+        user = _userService.updateUsername("New Username", user);
+
+        expect(user.getUsername()).not.toBe("RodrigoMijangos");
+        expect(user.getUsername()).toBe("New Username");
+    });
+
+    test('4. Getting usernames from list of users', () => {
+        const user1 = _userService.create(1, "Name 1", "Username1");
+        const user2 = _userService.create(2, "Name 2", "Username2");
+        const user3 = _userService.create(3, "Name 3", "Username3");
+
+        expect(user1.getUsername()).toBe("Username1");
+        expect(user2.getUsername()).toBe("Username2");
+        expect(user3.getUsername()).toBe("Username3");
+
+        const USERNAMES = _userService.getAllUsernames([user1, user2, user3]);
+
+        expect(USERNAMES[0]).toBe("Username1")
+        expect(USERNAMES[1]).toBe("Username2")
+        expect(USERNAMES[2]).toBe("Username3")
+
+    });
 });
